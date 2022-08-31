f1fcbbb81aa5e44065addb37939c4ef9e277d22a
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-31 11:28:57 +0530

    Minimise blog re-renders
    Blogs component only stores the blog id so when a
    blog is created/liked/delete all blogs does not
    need to re-render.

--- src/components/Blog/Blogs.tsx
+++ src/components/Blog/Blogs.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 import Blog             from "./Blog";
-import {selectBlogs}    from "../../reducers/blogReducer";
+import {selectBlogIds}  from "../../reducers/blogReducer";
 import {useAppSelector} from "../../hooks";
 
 const Blogs = React.memo(() => {
-  const {blogs} = useAppSelector(selectBlogs)
-  if (!blogs) return <div>Loading</div>
+  const blogIds = useAppSelector(selectBlogIds)
+  if (!blogIds) return <div>Loading</div>
   return (
     <>
-      {blogs.map(blog => <Blog key={blog!.id} blog={blog}/>)}
+      {blogIds.map(id => <Blog key={id} blogId={id}/>)}
     </>
   )
 })
